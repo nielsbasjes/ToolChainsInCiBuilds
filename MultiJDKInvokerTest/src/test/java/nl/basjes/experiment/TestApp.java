c2f0f489034cc466687fad4a26917a30544450f0
Niels Basjes <niels@basjes.nl>
2023-03-12 09:14:14 +0100

    Added demo on running the same IT test under different Java versions

---
+++ MultiJDKInvokerTest/src/test/java/nl/basjes/experiment/TestApp.java
@@ -0,0 +1,19 @@
+package nl.basjes.experiment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestApp {
+
+    @Test
+    void testSomething() {
+        assertEquals("Hello", new App().doSomething());
+    }
+
+    @Test
+    void showVersion() {
+        System.out.println("Java version during testing: " + System.getProperty("java.version"));
+    }
+
+}
