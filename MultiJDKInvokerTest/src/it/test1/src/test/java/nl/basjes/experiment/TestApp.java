8a562207947567819715de7ec78a8f85c7155cff
Niels Basjes <niels@basjes.nl>
2023-04-05 14:38:10 +0200

    fix: Cleanup of multi JDK testing.

--- MultiJDKInvokerTest/src/it/test1/src/test/java/nl/basjes/experiment/TestApp.java
+++ MultiJDKInvokerTest/src/it/test1/src/test/java/nl/basjes/experiment/TestApp.java
@@ -1,8 +1,14 @@
 package nl.basjes.experiment;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class TestApp {
 
@@ -12,9 +18,23 @@ class TestApp {
     }
 
     @Test
+    void logProperties() {
+        Logger logger = LoggerFactory.getLogger("Properties");
+        List<String> allKeyNames = System.getProperties().keySet().stream().map(Object::toString).sorted().collect(Collectors.toList());
+
+        int maxKeyLen = allKeyNames.stream().mapToInt(String::length).max().orElse(0);
+        for (Object key : allKeyNames) {
+            String keyName = key.toString();
+            logger.info("{}", String.format("%-" + maxKeyLen + "s ==> %s", keyName, System.getProperties().getProperty(key.toString())));
+        }
+    }
+
+    @Test
     void testVersion() {
         System.out.println("Java version during testing (code = "+Version.getCodeVersion()+"): " + Version.getJavaVersion());
-        assertEquals(System.getProperty("buildWithJDK"), Version.getJavaMajorVersion(), "Wrong major Java version was used");
+        String integrationTestingJDK= System.getProperty("integrationTestingJDK");
+        assertNotNull(integrationTestingJDK, "Property integrationTestingJDK was not set");
+        assertEquals(integrationTestingJDK, Version.getJavaMajorVersion(), "Wrong major Java version was used");
     }
 
 }
